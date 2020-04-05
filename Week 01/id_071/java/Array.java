b591bf74e423a038285573706c87148afd350ae3
feng003 <411152544@qq.com>
2020-04-05 17:42:45 +0800

    add java

---
+++ Week 01/id_071/java/Array.java
@@ -0,0 +1,17 @@
+package algorithm004-01.Week 01.id_071.java;
+public class Array {
+
+    public int data[];
+
+    private int n;
+
+    private int count;
+
+    public Array(int capacity) {
+        this.data = new int[capacity];
+        this.n = capacity;
+        this.count = 0;
+    }
+
+    
+}
---
+++ Week 01/id_071/java/SinglyLinkedList.java
@@ -0,0 +1,5 @@
+package algorithm004-01.Week 01.id_071.java;
+
+public class SinglyLinkedList {
+
+}
